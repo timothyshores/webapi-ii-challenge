0c522d5dcc63be2a5ea244d73e05357bca818f59
Timothy Shores <timothymshores@gmail.com>
2019-04-23 15:37:45 -0500

    Configure API to handle GET /api/posts/:id

--- router.js
+++ router.js
@@ -14,4 +14,20 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const data = await db.findById(req.params.id);
+
+        if (data) {
+            res.status(200).json(data);
+        } else {
+            const message404 = { message: "The post with the specified ID does not exist." }
+            res.status(404).json(message404);
+        }
+    } catch (error) {
+        const message500 = { error: "The post information could not be retrieved." }
+        res.status(500).json(message500);
+    }
+});
+
 module.exports = router;
